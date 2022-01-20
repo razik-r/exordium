ccc9735ce992bb23abc548c7b25ec2afba503eac
razik-r <96005647+razik-r@users.noreply.github.com>
2022-01-20 23:31:20 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+var myCarousel = document.querySelector('#carouselExampleCaptions')
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 3000,
+    wrap: true
+  })
+
+  var myCarousell = document.querySelector('#carouselExampleCaption')
+var carouseln = new bootstrap.Carousel(myCarousell, {
+    interval: 3000,
+    wrap: true
+  })
